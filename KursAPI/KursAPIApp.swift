dd20a2da07ae326c37b33900b0605b3e18eaa643
Christer Lindqvist <christer@festinafinance.com>
2023-12-02 10:58:58 +0100

    Initial Commit

---
+++ KursAPI/KursAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KursAPIApp.swift
+//  KursAPI
+//
+//  Created by Christer Lindqvist on 2023-12-02.
+//
+
+import SwiftUI
+
+@main
+struct KursAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
